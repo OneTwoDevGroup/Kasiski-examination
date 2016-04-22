8190505654cfd3a5e2961ff88e2e2a73e5269173
Дмитрий Куликов <kulikovdmitriyarkadevich@gmail.com>
2016-04-22 19:44:20 +0300

    Добавлены динамические структуры и их удаление, все вынесено в функцию Kasiski,  придуман коэффициент h, массив которых получаем на выходе

--- Kasici/kasiski_examination.cpp
+++ Kasici/kasiski_examination.cpp
@@ -1,93 +1,96 @@
 #include "stdio.h"
 #include "string.h"
+#include "malloc.h"
 #define N 5000
 #define r 256// ASCII
 #define R 26 //Мощность русского языка
-
-int main()
+int size;
+char alf[R] = { 'a','b','c','d','e','f','h','i','j','k','l','m','n','o','p','r','s','t','u','w','v','q','z','g' ,'x' };//буквы английского алфавита
+float* Kasiski()
 {
 	FILE* fp1;
 	fp1 = fopen("input.txt", "r");
-	char matched[N][50]; //массив с совпавшими значениями
-	int max_match_sum = 0; //кол-во одинаковых совпадений
-	int max_match_ind;// индекс максимального совпадения кол-ва
-	int s;
-	int i = 0, j, l, g, z,k=0; //счетчики
-	char S[N]; //строка с шифром//место поиска
-
-	while((S[k]=fgetc(fp1))&&(S[k]!=EOF))
-		k++;
+	fseek(fp1, 0, SEEK_END);
+	size = ftell(fp1);
+	fseek(fp1, 0, SEEK_SET);
+	char buff[100];										//Для копирование строк
+	int** lenght_distance;								//в lenght_distane[][0] заносится длина совпадения, в lenght_distance[][1] расстояние между совпадениями
+	char **matched;										//массив с совпавшими значениями
+	int max_match_sum = 0;								//кол-во одинаковых совпадений
+	int max_match_ind;									// индекс максимального совпадения кол-ва
+	int* count_matched;									// массив кол-ва совпадений
+	int w = 0;											// номер совпадения
+	int max_lenght_matched = 0;							//максимальное совпадение по длине
+	int max_m = -1;										//индекс; максимального совпадения
+	int i = 0, j, l, g, z,s;							//счетчики
+	char *S;											//строка с шифром, место поиска
 	
-	char alf[R] = { 'a','b','c','d','e','f','h','i','j','k','l','m','n','o','p','r','s','t','u','w','v','q','z','g' ,'x' };//буквы английского алфавита
-
-	int lenght_distance[N][2] = { 0 };//в lenght_distane[][0] заносится длина совпадения, в lenght_distance[][1] расстояние между совпадениями
-	int w = 0; // номер совпадения
-	int max_lenght_matched = 0; //максимальное совпадение по длине
-	int max_m = -1; //индекс; максимального совпадения
-	for (i = 0; i < R; i++)//i-прогоняемся по всем символам алфавита
+	i = 0;
+	S = (char*)malloc(1);
+	while (i <size)										//динамически заносим в память
 	{
-		for (j = 0; j < k; j++) // ищем все символы Alf[i] в S[]
+		i++;
+		S = (char*)realloc(S, i*(sizeof(char)));
+		*(S + i - 1) = fgetc(fp1);
+	}
+
+	
+	matched = (char**)malloc(sizeof(char*));
+	lenght_distance = (int**)malloc(sizeof(int*));
+
+	for (i = 0; i < R; i++)								//i-прогоняемся по всем символам алфавита
+	{
+		for (j = 0; j < size; j++)						// ищем все символы Alf[i] в S[]
 			if (S[j] == alf[i])
-				for (l = j+1; l < k; l++)// l - ищем все символы Alf[i] в строке S[j+]
+				for (l = j + 1; l < size; l++)			// l - ищем все символы Alf[i] в строке S[j+]
 				{
 					if (S[l] == alf[i])
 					{
 						g = 0;
-						while (((l + g) < k) && ((j + g) < k) && (S[l + g] == S[j + g]))
-							g++;                                                          //g считаем сколько символов совпало
-						if (g > 2)
+						while (((l + g) < size) && ((j + g) < size) && ((S[l + g] == S[j + g])/*||( S[l+g]<97||S[j+g]<97)*/))
+							g++;						//g считаем сколько символов совпало
+						if (g>(size/3000)+2)
 						{
+							matched = (char**)realloc(matched, (sizeof(char*)*(w + 1)));
+							matched[w] = (char*)malloc(1);
 							for (z = 0; z < g; z++)
+							{
+								matched[w] = (char*)realloc(matched[w], sizeof(char)*(z + 2));
 								matched[w][z] = S[l + z];
-							
+							}
 							matched[w][z] = '\0';
+							lenght_distance = (int**)realloc(lenght_distance, sizeof(int*)*(w + 1));
+							lenght_distance[w] = (int*)malloc(2 * (sizeof(int)));
 							lenght_distance[w][0] = g;
 							lenght_distance[w][1] = l - j; // расстояние между совпадениями
 							w++;
 						}
-
-
 					}
 				}
 	}
-	char buff[50];
-	for (j = 0; j < w-1; j++) 
+
+	max_lenght_matched = 0;
+	for (j = 0; j < w ; j++)
 	{
-		max_lenght_matched = 0;
-		for (i = j; i < w; i++)
 			if (lenght_distance[i][0]>max_lenght_matched)
 			{
-				max_lenght_matched = lenght_distance[i][0];      // выбираем самое максимальное совпадение и растояние между ними( cчитаем его 100%)
-				max_m = i;
+				max_lenght_matched = lenght_distance[i][0];// выбираем самое максимальное совпадение и растояние между ними( cчитаем его 100%)
+				max_m = j;
 			}
-		max_lenght_matched = lenght_distance[j][0];
-		lenght_distance[j][0] = lenght_distance[max_m][0];    //сортируем совпадения по длине, поэтому меняем длину и растояние местами.
-		lenght_distance[max_m][0] = max_lenght_matched;
-		max_lenght_matched = lenght_distance[j][1];
-		lenght_distance[j][1] = lenght_distance[max_m][1];
-		lenght_distance[max_m][1] = max_lenght_matched;
-		strcpy(buff, matched[j]);
-		strcpy(matched[j], matched[max_m]);
-		strcpy(matched[max_m], buff);
-
 	}
-	
-	for (i = 0; i < w - 1; i++)
-	{
-		s = 0;
-		
-		for (j = i; j < w; j++)
-		{
-
+	count_matched =(int*) calloc(w,sizeof(int));
+	for (i = 0; i < w -1; i++)
+	{	
+		s = 1;
+		for (j = i ; j < w; j++)
+		{	
 			z = 0;
-			while (matched[i][z] && matched[j][z] && (matched[i][z] == matched[j][z]))
+			if (!strcmp(matched[i], matched[j]))
 			{
-				z++;
-			}
-			if ((matched[i][z] == '\0') && (matched[j][z] == '\0'))
+				count_matched[i]++;
+				count_matched[j]++;
 				s++;
-
-
+			}
 		}
 		if ((max_match_sum < s))
 		{
@@ -95,28 +98,64 @@ int main()
 			max_match_ind = i;
 		}
 	}
+
 	for (i = 0; i < w; i++)
 	{
-		printf("%s ", matched[i]);
+		printf("         %s", matched[i]);
 	}
-	float h;
-	for (j =1 ; j < 100; j++)
-	{
-		 h = 0;
-		for (i = 0; i < w; i++)
-		{
-			if ((lenght_distance[i][1] % j) == 0)
-				h++;
-		}
-		
-		h /= w;
-		printf("%d %f \%",j,h*100);
 
+	float h[N];
+
+	for (j = 2; j < 20; j++)
+	{
+		h[j] = 0;
+			for (i = 0; i < w; i++)
+			{
+				if ((lenght_distance[i][1] % j) == 0)
+				{
+					if (lenght_distance[i][0]>4)
+					{
+						if(count_matched[i]>2*max_match_sum/3)
+						h[j] += 4.0*lenght_distance[i][0];
+						else
+						{
+							h[j] += 2.0*lenght_distance[i][0];
+						}
+					}
+					else
+					{
+						
+
+						if (count_matched[i]>2 * max_match_sum / 3)
+							h[j] += 2.0*lenght_distance[i][0];
+						else
+							h[j] += 1.0*lenght_distance[i][0];
+					}
+				}
+
+			}
+		printf("%d %f \%", j, h[j]);
 	}
 	printf("%d %s", max_match_sum, matched[max_match_ind]);
 
+	//ЗАЧИСТКА
+	free(count_matched);
+	free(S);
+	for (i = 0; i < w; i++)
+	{
+		free(matched[i]);
+		free(lenght_distance[i]);
+	}
+	free(matched);
+	free(lenght_distance);
 
+	return h;
+}
+
+
+
+int main()
+{
+	Kasiski();
 	return 0;
-
-
 }
